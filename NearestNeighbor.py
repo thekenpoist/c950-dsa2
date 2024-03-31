1cf581cb8a4fce67318bc51831e75ce55e2e2a29
Steve Hull <hullstevenj@gmail.com>
2024-03-30 18:33:58 -0700

    TruckLoader set up

--- NearestNeighbor.py
+++ NearestNeighbor.py
@@ -3,6 +3,7 @@ from DistanceBetweenAddresses import *
 from PackageLoader import *
 import datetime
 import DeliveryTruck
+from TruckLoader import *
 
 
 class NearestNeighbor:
@@ -14,18 +15,10 @@ class NearestNeighbor:
 
         distance_between_addreses = DistanceBetweenAddresses()
 
-        # [3, 8, 9, 10, 11, 12, 17, 18, 19, 22, 23, 24, 27, 35, 36, 38]
+        truck1, truck2, truck3 = TruckLoader().truck_loader()
 
-        truck1 = DeliveryTruck.DeliveryTruck("Truck One", datetime.timedelta(hours=8), "4001 South 700 East",
-                                             [1, 2, 4, 7, 13, 14, 15, 16, 20, 21, 29, 33, 34, 39, 40])
+        truck = truck3
 
-        truck2 = DeliveryTruck.DeliveryTruck("Truck Two", datetime.timedelta(hours=10, minutes=20), "4001 South 700 East",
-                                             [3, 8, 9, 10, 11, 17, 18, 19, 22, 23, 24, 27, 35, 36, 38])
-
-        truck3 = DeliveryTruck.DeliveryTruck("Truck Three", datetime.timedelta(hours=9, minutes=5), "4001 South 700 East",
-                                             [5, 6, 12, 25, 26, 28, 30, 31, 32, 37])
-
-        truck = truck2
         current_address = truck.start_location
         travel_time = truck.start_time
         total_distance = 0
@@ -74,10 +67,13 @@ class NearestNeighbor:
                                   weight, notes, truck.truck_number, str(travel_time), status)
 
             package_loader.insert(ID, p)
-            # print(truck.packages)
             truck.packages.pop(j)
-            # print(truck.packages)
 
             print(temp_distance, " --- ", p)
+
+        # Send Truck 1 and driver back to The Hub so Truck 2 can start deliveries
+        if truck:
+            total_distance = total_distance + float(distance_between_addreses.address_distance(
+                distance_file, current_address, truck.start_location))
 
         return total_distance
--- TruckLoader.py
+++ TruckLoader.py
@@ -9,14 +9,13 @@ class TruckLoader:
 
     def truck_loader(self):
 
-        truck1 = DeliveryTruck.DeliveryTruck("Truck One", 16, datetime.timedelta(hours=8), "4001 South 700 East",
-                                             [1, 2, 4, 7, 13, 14, 15, 16, 20, 21, 29, 33, 34, 39, 40])
+        truck1 = DeliveryTruck.DeliveryTruck("Truck One", datetime.timedelta(hours=8), "4001 South 700 East",
+                                             [1, 2, 4, 7, 12, 13, 14, 15, 16, 20, 21, 29, 33, 34, 39, 40])
 
-        print(type(truck1))
-        # return truck1
+        truck2 = DeliveryTruck.DeliveryTruck("Truck Two", datetime.timedelta(hours=10, minutes=20), "4001 South 700 East",
+                                             [3, 8, 9, 10, 11, 17, 18, 19, 22, 23, 24, 27, 35, 36, 38])
 
-        # truck2 = DeliveryTruck(16, datetime.timedelta(hours=9, minutes=5), "4001 South 700 East",
-        # "4001 South 700 East", [6,25,30,31,37],[3,18,36,38,11,12,17,21,22,23,24])
+        truck3 = DeliveryTruck.DeliveryTruck("Truck Three", datetime.timedelta(hours=9, minutes=5), "4001 South 700 East",
+                                             [5, 6, 25, 26, 28, 30, 31, 32, 37])
 
-        # truck3 = DeliveryTruck(16, datetime.timedelta(hours=12), "4001 South 700 East",
-        # "4001 South 700 East", [],[9,26,27,28,32,33,35,39])
+        return truck1, truck2, truck3
